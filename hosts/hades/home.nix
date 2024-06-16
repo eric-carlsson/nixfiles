ab20b711d470fa52c054dcd32c150cbf5cb52cf5
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2024-06-16 20:16:11 +0200

    Keyboard shortcut for vscode

--- hosts/hades/home.nix
+++ hosts/hades/home.nix
@@ -68,6 +68,7 @@
       "org/gnome/settings-daemon/plugins/media-keys" = {
         custom-keybindings = [
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
         ];
         www = ["<Super>b"];
         home = ["<Super>f"];
@@ -77,6 +78,12 @@
         binding = "<Super>t";
         command = "kgx";
         name = "GNOME Console";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+        binding = "<Super>c";
+        command = "code";
+        name = "VS Code";
       };
     };
   };
