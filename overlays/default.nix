c8b5d16619dd3b8eae2a4d734d936cc006f63ed2
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2024-12-02 22:17:09 +0100

    build crossplane-cli from stable

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,14 +14,6 @@
       kind = pkgs-unstable.kind;
       clusterctl = pkgs-unstable.clusterctl;
       go_1_23 = pkgs-unstable.go_1_23;
-      crossplane-cli = pkgs-unstable.crossplane-cli.overrideAttrs (old: {
-        src = prev.fetchFromGitHub {
-          owner = "crossplane";
-          repo = "crossplane";
-          rev = "release-1.17";
-          hash = "sha256-Fr3lVVghnricQ5bOFVSQgbrJshC9r0159xsm0FLAiqQ=";
-        };
-      });
       velero = pkgs-unstable.velero;
     };
 
