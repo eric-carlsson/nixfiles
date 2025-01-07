0b45e45ff03a863ae111eabdf32bb916e260d023
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2025-01-07 21:42:20 +0100

    zeus: set default mime apps

--- hosts/zeus/home.nix
+++ hosts/zeus/home.nix
@@ -25,6 +25,38 @@
           gnomeExtensions.dash-to-dock
         ];
 
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "x-scheme-handler/http" = "firefox.desktop";
+            "text/html" = "firefox.desktop";
+            "application/xhtml+xml" = "firefox.desktop";
+            "x-scheme-handler/https" = "firefox.desktop";
+            "image/jpeg" = "org.gnome.Loupe.desktop";
+            "image/png" = "org.gnome.Loupe.desktop";
+            "image/gif" = "org.gnome.Loupe.desktop";
+            "image/webp" = "org.gnome.Loupe.desktop";
+            "image/tiff" = "org.gnome.Loupe.desktop";
+            "image/x-tga" = "org.gnome.Loupe.desktop";
+            "image/vnd-ms.dds" = "org.gnome.Loupe.desktop";
+            "image/x-dds" = "org.gnome.Loupe.desktop";
+            "image/bmp" = "org.gnome.Loupe.desktop";
+            "image/vnd.microsoft.icon" = "org.gnome.Loupe.desktop";
+            "image/vnd.radiance" = "org.gnome.Loupe.desktop";
+            "image/x-exr" = "org.gnome.Loupe.desktop";
+            "image/x-portable-bitmap" = "org.gnome.Loupe.desktop";
+            "image/x-portable-graymap" = "org.gnome.Loupe.desktop";
+            "image/x-portable-pixmap" = "org.gnome.Loupe.desktop";
+            "image/x-portable-anymap" = "org.gnome.Loupe.desktop";
+            "image/x-qoi" = "org.gnome.Loupe.desktop";
+            "image/svg+xml" = "org.gnome.Loupe.desktop";
+            "image/svg+xml-compressed" = "org.gnome.Loupe.desktop";
+            "image/avif" = "org.gnome.Loupe.desktop";
+            "image/heic" = "org.gnome.Loupe.desktop";
+            "image/jxl" = "org.gnome.Loupe.desktop";
+          };
+        };
+
         dconf = {
           enable = true;
           settings = {
