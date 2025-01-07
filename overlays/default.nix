f522317e747e71d53be6a4310f2883f0c6dbda6f
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2025-01-07 23:12:27 +0100

    add overlay for upstream pop-shell gnome 47 fix

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,6 +14,18 @@
       clusterctl = pkgs-unstable.clusterctl;
       go_1_23 = pkgs-unstable.go_1_23;
       velero = pkgs-unstable.velero;
+      gnomeExtensions =
+        prev.gnomeExtensions
+        // {
+          pop-shell = prev.gnomeExtensions.pop-shell.overrideAttrs (old: {
+            src = prev.fetchFromGitHub {
+              owner = "pop-os";
+              repo = "shell";
+              rev = "104269ede04d52caf98734b199d960a3b25b88df";
+              hash = "sha256-rBu/Nn7e03Pvw0oZDL6t+Ms0nesCyOm4GiFY6aYM+HI=";
+            };
+          });
+        };
     };
 
     additions = final: prev: import ../pkgs final.pkgs;
