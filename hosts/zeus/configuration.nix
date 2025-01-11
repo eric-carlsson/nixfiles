0ec13494a190e45ca2df146b384e1b4be6713da9
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2025-01-11 19:32:56 +0100

    zeus: add workaround for crash on suspend when kvm enabled

--- hosts/zeus/configuration.nix
+++ hosts/zeus/configuration.nix
@@ -11,6 +11,10 @@
     # disable apst for kingston nvme
     boot.kernelParams = ["nvme_core.default_ps_max_latency_us=0"];
 
+    # workaround for issue with suspend when kvm is enabled
+    # see https://github.com/NixOS/nixpkgs/issues/369376
+    systemd.services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+
     # Graphics drivers
     hardware.graphics.enable = true;
     services.xserver.videoDrivers = ["nvidia"];
