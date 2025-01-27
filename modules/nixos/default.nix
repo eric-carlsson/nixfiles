661f30d8c6c1befd1146e01fbc9396794664e641
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2025-01-27 21:35:49 +0100

    nixos: enable automatic gc and optimise

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
   config = {
     nix.settings.experimental-features = ["nix-command" "flakes"];
+    nix.optimise.automatic = true;
+    nix.gc.automatic = true;
 
     boot = {
       loader = {
