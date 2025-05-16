ed9e537101a77711669a155eadb4d83fa27e8d08
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2025-05-16 09:07:39 +0200

    5CG2149Z4L: add kubectl-validate

--- hosts/5CG2149Z4L/home.nix
+++ hosts/5CG2149Z4L/home.nix
@@ -13,6 +13,7 @@
       kubelogin
       kubernetes-helm
       kubectl
+      kubectl-validate
       slack
       (azure-cli.withExtensions [azure-cli.extensions.aks-preview azure-cli.extensions.k8s-extension azure-cli.extensions.quota])
       terraform
